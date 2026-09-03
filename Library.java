79c0468853a6b11292c43f1ccad32b0f486716f1
AERYTHIX <321923673+AERYTHIXU@users.noreply.github.com>
2026-09-03 17:18:51 -0500

    Resolve Library merge conflict

--- Library.java
+++ Library.java
@@ -1,5 +1,6 @@
 public class Library {
-
+    
     private Author[] authors = new Author[]{};
-
+    private Book[] books = new Book[]{};
+    private Publisher[] publishers = new Publisher[]{};
 }
